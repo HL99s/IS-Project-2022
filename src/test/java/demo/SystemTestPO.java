173df70a78656f65e14363f97c57001f2b32b105
HL99s <65026011+HL99s@users.noreply.github.com>
2022-01-14 18:12:59 +0100

    Changes by Utente

--- src/test/java/demo/SystemTestPO.java
+++ src/test/java/demo/SystemTestPO.java
@@ -1,13 +1,44 @@
 package demo;
 
-import demo.po.AddPersonPO;
-import demo.po.EmptyListPO;
+import demo.po.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
 import static org.junit.Assert.*;
 
 public class SystemTestPO extends BaseTest{
+
+    @Test
+    public void testValidLogin(){
+        driver.get("http://localhost:8080/");
+        LoginPO loginPage = new LoginPO(driver);
+
+        loginPage.enterCredentials("admin", "admin");
+        HomePO homePage = loginPage.validSubmit();
+
+        assertEquals("Hi, Welcome to Mentcare system", homePage.getMessage());
+    }
+
+    @Test
+    public void testInvalidLogin(){
+        driver.get("http://localhost:8080/");
+        LoginPO loginPage = new LoginPO(driver);
+
+        loginPage.enterCredentials("admin", "pen");
+        InvalidLoginPO reLogPage = loginPage.invalidSubmit();
+
+        assertEquals("Username or Password incorrect, Please try again", reLogPage.getMessage());
+
+        reLogPage.enterCredentials("apple", "a");
+        InvalidLoginPO reLogPage1 = reLogPage.invalidSubmit();
+
+        assertEquals("Username or Password incorrect, Please try again", reLogPage1.getMessage());
+
+        reLogPage1.enterCredentials("admin", "admin");
+        HomePO homePage = reLogPage1.validSubmit();
+
+        assertEquals("Hi, Welcome to Mentcare system", homePage.getMessage());
+    }
 
     @Test
     public void testAddPerson(){
