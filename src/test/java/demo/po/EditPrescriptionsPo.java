15894a8c609b68e3cc185fedcc5dd04c72d84395
HL99s <65026011+HL99s@users.noreply.github.com>
2022-01-15 17:43:31 +0100

    Project 15/01/2022

---
+++ src/test/java/demo/po/EditPrescriptionsPo.java
@@ -0,0 +1,48 @@
+package demo.po;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class EditPrescriptionsPo extends PageObject{
+
+    public EditPrescriptionsPo(WebDriver driver){
+        super(driver);
+    }
+
+    @FindBy(xpath = "/html[1]/body[1]/h1[1]")
+    private WebElement initialMessage;
+
+    @FindBy(name = "username")
+    private WebElement username;
+
+    @FindBy(name = "password")
+    private WebElement password;
+
+    @FindBy(xpath = "/html[1]/body[1]/form[1]/input[3]")
+    private WebElement submit;
+
+    public String getMessage(){
+        return this.initialMessage.getText();
+    }
+
+    public void enterCredentials(String username, String password){
+        this.username.clear();
+        this.password.clear();
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+    }
+
+    public HomePO validSubmit(){
+        submit.click();
+        return new HomePO(driver);
+    }
+
+    public InvalidLoginPO invalidSubmit(){
+        submit.click();
+        return new InvalidLoginPO(driver);
+    }
+
+
+
+}
--- src/test/java/demo/po/ShowPatientsPO.java
+++ src/test/java/demo/po/ShowPatientsPO.java
@@ -14,22 +14,38 @@ public class ShowPatientsPO extends PageObject{
     @FindBy(xpath = "/html[1]/body[1]/h1[1]")
     private WebElement titleMessage;
 
+    @FindBy(xpath = "/html[1]/body[1]/table[1]/tbody[1]/tr[1]/td[6]/a[1]")
+    private WebElement showPatientInfoLink;
+
+    @FindBy(xpath = "/html[1]/body[1]/form[1]/input[1]")
+    private WebElement backToHomeButton;
+
+    @FindBy(xpath = "/html[1]/body[1]/table[1]/tbody[1]/tr[1]/td[1]")
+    private WebElement firstRowId;
+
     @FindBy(tagName = "table")
     private WebElement table;
 
-    @FindBy(xpath = "//table//tbody//td[2]")
-    private WebElement firstRow;
-
-    public String getFirstRowName(){
-        return firstRow.getText();
-    }
-
-    public int getTableSize(){
-        return table.findElements(By.tagName("tr")).size();
-    }
-
     public String getMessage(){
         return titleMessage.getText();
+    }
+
+    public ShowPatientsInfoPO showPatientsInfo(){
+        showPatientInfoLink.click();
+        return new ShowPatientsInfoPO(driver);
+    }
+
+    public HomePO backToHome(){
+        backToHomeButton.click();
+        return new HomePO(driver);
+    }
+
+    public String getFirstRowId(){
+        return firstRowId.getText();
+    }
+
+    public Integer getTableSize(){
+        return table.findElements(By.tagName("tr")).size();
     }
 
 
