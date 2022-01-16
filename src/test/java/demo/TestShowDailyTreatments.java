7c3f0d69c9673e559d79f3b3e57c9b84a2896249
HL99s <65026011+HL99s@users.noreply.github.com>
2022-01-16 15:42:27 +0100

    Changes by Utente

--- src/test/java/demo/TestShowDailyTreatments.java
+++ src/test/java/demo/TestShowDailyTreatments.java
@@ -10,27 +10,6 @@ import static org.junit.Assert.assertEquals;
 
 public class TestShowDailyTreatments extends BaseTest{
 
-    @Test
-    public void testShowTreatments(){
-        driver.get("http://localhost:8080/");
-        LoginPO loginPage = new LoginPO(driver);
-        assertEquals("Hi, Please Log-In to Mentcare system", loginPage.getMessage());
-
-        loginPage.enterCredentials("admin", "admin");
-        HomePO homePage = loginPage.validSubmit();
-
-        assertEquals("Hi, Welcome to Mentcare system", homePage.getMessage());
-
-        ShowTreatmentsPO treatmentsPage = homePage.showTreatmentsSubmit();
-
-        assertEquals("Treatments list", treatmentsPage.getMessage());
-        assertEquals(13, treatmentsPage.getTableSize());
-        assertEquals("1", treatmentsPage.getFirstRowId());
-
-        HomePO homePage1 = treatmentsPage.backToHome();
-
-        assertEquals("Hi, Welcome to Mentcare system", homePage1.getMessage());
-    }
 
     @Test
     public void testShowDailyTreatments(){
