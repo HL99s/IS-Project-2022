c3ba430d231b708c8ef7b10936db87df7bc5891d
Ricky <riccardo.bologna99@gmail.com>
2022-01-21 16:57:38 +0100

    finish with coverage

--- src/main/java/demo/TreatmentWithPatient.java
+++ src/main/java/demo/TreatmentWithPatient.java
@@ -1,18 +1,10 @@
 package demo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Date;
-
-
 public class TreatmentWithPatient {
 
     private Treatment treatment;
     private Patient patient;
 
-    protected TreatmentWithPatient() {}
 
     public TreatmentWithPatient(Treatment treatment, Patient patient) {
         this.treatment = treatment;
